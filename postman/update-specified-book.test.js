7ec7054caaf62e2f2b3f81af983bc81ec6d88493
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-03-22 22:46:53 +0800

    update the testing for user failed to fill the name error

--- postman/update-specified-book.test.js
+++ postman/update-specified-book.test.js
@@ -3,6 +3,10 @@ pm.test('response status code should have 200 value', () => {
   pm.response.to.have.status(200);
 });
 
+pm.test('response status code should have 400 value if the user is not filling the name', () => {
+  pm.response.to.have.status(400);
+});
+
 pm.test('response Content-Type header should have application/json value', () => {
   pm.expect(pm.response.headers.get('Content-Type')).to.equals('application/json; charset=utf-8');
 });
@@ -14,11 +18,19 @@ pm.test('response body should be an object', () => {
 
 pm.test('response body should have correct property and value', () => {
   const responseJson = pm.response.json();
+  const checkStatus = pm.response.code == 200 ? true : false;
 
-  pm.expect(responseJson).to.have.ownProperty('status');
-  pm.expect(responseJson.status).to.equals('success');
-  pm.expect(responseJson).to.have.ownProperty('message');
-  pm.expect(responseJson.message).to.equals('Buku berhasil diperbarui');
+  if (checkStatus) {
+    pm.expect(responseJson).to.have.ownProperty('status');
+    pm.expect(responseJson.status).to.equals('success');
+    pm.expect(responseJson).to.have.ownProperty('message');
+    pm.expect(responseJson.message).to.equals('Buku berhasil diperbarui');
+  } else {
+    pm.expect(responseJson).to.have.ownProperty('status');
+    pm.expect(responseJson.status).to.equals('fail');
+    pm.expect(responseJson).to.have.ownProperty('message');
+    pm.expect(responseJson.message).to.equals('Gagal memperbarui buku. Mohon isi nama buku');
+  }
 });
 
 pm.test('when request the updated book', () => {
